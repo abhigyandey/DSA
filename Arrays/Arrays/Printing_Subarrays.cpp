999ce5d5ccc7d8198c4fb1d2eb5b4570d8872ba7
abhigyandey <abhigyandey94@gmail.com>
2025-11-20 12:44:54 +0530

    Add files via upload

---
+++ Arrays/Arrays/Printing_Subarrays.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printSubArrays(int arr[], int n){
+    
+    for(int i=0;i < n; i++){
+        for(int j=0; j <n ; j++){
+            //cout << "(" << i << "," << j << "),";
+            
+            for(int k= i; k <= j; k++){
+                cout << arr[k] << ",";
+            }
+            cout << endl;
+        }
+
+    }
+    
+}
+
+int main() {
+	int arr[] = {10,20,30,40,50,60};
+	int n = sizeof(arr)/ sizeof(int);
+	
+	printSubArrays(arr,n);
+	
+	return 0;
+	
+
+}
